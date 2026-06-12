1eea06f6829f37fdf42a72c0b9ceab98571416d5
Devin Xie <devinxie418@gmail.com>
2026-06-11 23:35:42 -0400

    feat: add session model for refresh tokens

---
+++ prisma/migrations/20260612033521_add_sessions/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "hashedToken" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
