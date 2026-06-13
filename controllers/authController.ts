fd7781b8001706fe8f864e067250526046fa7384
Devin Xie <devinxie418@gmail.com>
2026-06-12 21:41:14 -0400

    feat: logout, getCurrentUser

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,10 +1,11 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //config
 import { prisma } from "../config/prismaClient";
 
 // services
-import { issueTokens, createSession } from "../services/auth";
+import { issueTokens, createSession, deleteSession } from "../services/auth";
 
 // types
 import type { Request, Response } from "express";
@@ -86,7 +87,7 @@ async function login(req: Request, res: Response) {
             message: "User successfully logged in!",
             accessToken,
             user: userDetails
-        })
+        });
     } catch(err: any) {
         console.error("Error in login: ", err);
         return res.status(500).json({
@@ -97,14 +98,33 @@ async function login(req: Request, res: Response) {
 
 async function logout(req: Request, res: Response) {
     try {
-
+        const refreshToken = req.cookies.refreshToken;
+        await deleteSession(refreshToken);
+        
+        res.clearCookie("refreshToken");
+        return res.status(200).json({
+            message: "User successfully logged out!"
+        });
     } catch(err: any) {
         console.error("Error in signup: ", err);
     }
 };
 
+async function getCurrentUser(req: Request, res: Response) {
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) return res.status(400).json({ error: "Token is missing." })
+
+    const accessToken = authHeader.split(" ")[1];
+    if(!accessToken) return res.status(401).json({ error: "Token is missing." });
+
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY!);
+    
+    return res.status(200).json({ user: decoded });
+}
+
 export const authController = {
     signup,
     login,
-    logout
+    logout,
+    getCurrentUser,
 }
--- routers/authRouter.ts
+++ routers/authRouter.ts
@@ -4,9 +4,12 @@ import { authController } from "../controllers/authController";
 // middleware
 import { authValidator } from "../middleware/authValidator";
 import { validateRequest } from "../middleware/validateRequest";
+import { verifyAuth } from "../middleware/verifyAuth";
 
 export const authRouter = Router();
 
 authRouter.post("/signup", authValidator.validateSignup, validateRequest, authController.signup);
 authRouter.post("/login", authValidator.validateLogin, validateRequest, authController.login);
 authRouter.get("/logout", authController.logout);
+
+authRouter.get("/me", verifyAuth, authController.getCurrentUser);
