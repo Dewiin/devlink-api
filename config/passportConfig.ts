fcee79746507a4bb56323a1c6bf71dc2227695cb
Devin Xie <devinxie418@gmail.com>
2026-06-18 12:27:17 -0400

    feat: config for local, google, and github strategies

---
+++ config/passportConfig.ts
@@ -0,0 +1,100 @@
+import { prisma } from "./prismaClient";
+import passport from "passport";
+import bcrypt from "bcryptjs";
+
+// strategies
+import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GithubStrategy } from "passport-github";
+
+// types
+import type { IVerifyOptions } from "passport-local";
+import type { VerifyCallback as GoogleVerifyCallback, Profile as GoogleProfile } from "passport-google-oauth20";
+import type { VerifyCallback as GithubVerifyCallback } from "passport-oauth2";
+import type { Profile as GithubProfile } from "passport-github";
+
+async function localVerifyFunction(
+    email: string, 
+    password: string, 
+    done: (error: any, user?: Express.User | false, options?: IVerifyOptions) => void 
+) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+    
+        if(!user) return done(null, false, { message: "User does not exist." });
+        if(!user.password) return done(null, false, { message: "Email associated with google account." });
+
+        const match = await bcrypt.compare(password, user.password);
+        if(!match) return done(null, false, { message: "Incorrect username or password" });
+
+        return done(null, user);
+    } catch(err: any) {
+        return done(err);
+    }
+}
+
+async function googleVerifyFunction(
+    accessToken: string, 
+    refreshToken: string, 
+    profile: GoogleProfile, 
+    cb: GoogleVerifyCallback
+) {
+    try {
+        const email = profile.emails?.[0]?.value;
+        if(!email) return cb(new Error("No email found in google profile."));
+
+        const user = await prisma.user.upsert({
+            where: { email },
+            update: { displayName: profile.displayName },
+            create: {
+                email,
+                displayName: profile.displayName,
+                provider: "GOOGLE"
+            }
+        });
+
+        return cb(null, user);
+    } catch(err: any) {
+        return cb(err);
+    }
+}
+
+async function githubVerifyFunction(
+    accessToken: string,
+    refreshToken: string,
+    profile: GithubProfile,
+    cb: GithubVerifyCallback
+) {
+    try {
+        const email = profile.emails?.[0]?.value;
+        if(!email) return cb(new Error("No email found in google profile."));
+
+        const user = await prisma.user.upsert({
+            where: { email },
+            update: { displayName: profile.displayName },
+            create: {
+                email,
+                displayName: profile.displayName,
+                provider: "GITHUB"
+            }
+        });
+
+        return cb(null, user);
+    } catch(err: any) {
+        return cb(err);
+    }
+}
+
+passport.use(new LocalStrategy(localVerifyFunction));
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: "http://www.example.com/auth/google/callback"
+}, googleVerifyFunction));
+passport.use(new GithubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+}, githubVerifyFunction));
