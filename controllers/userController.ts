7a1d0057377b6f521e0451db78633f13ca60084d
Devin Xie <devinxie418@gmail.com>
2026-06-21 11:01:08 -0400

    feat: get user's conversations

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -2,6 +2,7 @@ import { prisma } from "../config/prismaClient";
 
 // types
 import type { Request, Response } from "express"
+import type { User } from "../generated/prisma/client";
 
 async function getAllUsers(req: Request, res: Response) {
     try {
@@ -19,6 +20,34 @@ async function getAllUsers(req: Request, res: Response) {
     }
 }
 
+async function getUserChats(req: Request, res: Response) {
+    try {
+        const user = req.user as User;
+
+        const chats = await prisma.conversation.findMany({
+            where: {
+                participants: {
+                    some: { id: user.id },
+                }
+            },
+            include: {
+                participants: true
+            }
+        });
+
+        return res.status(200).json({
+            message: "Successfully fetched user's chats.",
+            chats
+        });
+    } catch(err: any) {
+        console.error("Error in getUserChats: ", err);
+        return res.status(500).json({
+            error: "Server error fetching user's chats." 
+        });
+    }
+}
+
 export const userController = {
     getAllUsers,
+    getUserChats
 }
--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { userController } from "../controllers/userController";
 
+// middleware
+import { verifyAuth } from "../middleware/verifyAuth";
+
 export const userRouter = Router();
 
 userRouter.get('/', userController.getAllUsers);
+userRouter.get('/chats', verifyAuth, userController.getUserChats);
